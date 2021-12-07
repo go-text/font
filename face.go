e030a25e63a9f906dca64b3e1a61ee6f9d04f5bf
Benoit KUGLER <x.ben.x@free.fr>
2021-12-07 20:05:50 +0100

    add nominal glyph method

--- face.go
+++ face.go
@@ -1,3 +1,9 @@
 package font
 
-type Face interface{}
+import "github.com/benoitkugler/textlayout/fonts"
+
+type Face interface {
+	// NominalGlyph returns the glyph used to represent the given rune,
+	// or false the rune is not supported.
+	NominalGlyph(r rune) (fonts.GID, bool)
+}
